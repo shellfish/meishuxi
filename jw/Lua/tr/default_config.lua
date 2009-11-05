4a1b638b6e9593c5f462aea5d00ddb690b1606c7
zhousiyv <zhousiyv@2b567cfb-dbe9-4a7d-8032-5a92f511a253>
2009-11-05 11:53:52 +0000

    clean finish
    git-svn-id: file:///mnt/libtree/archieve/svn/trunk@211 2b567cfb-dbe9-4a7d-8032-5a92f511a253

---
+++ jw/Lua/tr/default_config.lua
@@ -0,0 +1,33 @@
+
+module(...)
+-------------------------------------------------------------------------------
+-- Default configuration options
+-- @class table
+-- @name default_options
+-- @field DATABASE_SOURCE now default is jwdb
+-- @field DATABASE_USER jwuser
+-- @field DATABASE_PORT need't point( default is 5432)
+-- @field DATABASE_DRIVER default to postgres
+-- @field DATABASE_INITSTAT the statement will be executed when init db
+-- Like <code>SET search_path TO sth;</code>
+-- <hr />
+-- <h3> node loader </h3>
+-- @field NODE_LOAD_PATH the path will be use <b>AS</b> Virtual Loader 
+-- root dir
+-- <hr /><h3> Memcached related </h3>
+-- @field MEMCACHED_HOST N/A
+-- @field MEMCACHED_PORT N/A
+local default_options = {
+	DATABASE_SOURCE = "jwdb",
+	DATABASE_USER  = 'jwuser',
+	DATABASE_DRIVER = 'postgres',
+	DATABASE_INITSTAT = "SET search_path TO jw;",
+
+	AUTH_METHOD = "database",
+	AUTH_TOKEN_NAME = 'userHash',
+	AUTH_PASSWD_SALT = "5d41402abc4b2a76b9719d911017c592", -- 影子密码加密key
+
+	NODE_LOAD_PATH = '/home/zhousiyv/桌面/trunk/jw/Node',
+}
+
+return default_options
