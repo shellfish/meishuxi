5de1b44b102f02d45ac02309e92d3d158ef0f76d
zhousiyv <zhousiyv@2b567cfb-dbe9-4a7d-8032-5a92f511a253>
2009-11-14 16:32:42 +0000

    init 美术系 database
    git-svn-id: file:///mnt/libtree/archieve/svn/trunk@218 2b567cfb-dbe9-4a7d-8032-5a92f511a253

---
+++ meishu/Script/tab.sql
@@ -0,0 +1,79 @@
+
+BEGIN;
+
+--========= 创建模式 ===============
+DROP SCHEMA IF EXISTS meishuxi CASCADE;
+CREATE SCHEMA meishuxi;
+
+SET search_path TO meishuxi;
+
+-- 性别类型T_ISOSEX: ISO 5218-Codes for the representation of human sexes
+--  * 0 = not known,
+--  * 1 = male,
+--  * 2 = female,
+--  * 9 = not applicable.like the gov or a  coo.
+CREATE DOMAIN T_ISOSEX AS SMALLINT  CHECK (VALUE IN (0, 1, 2, 9)); 
+
+
+/*
+	DOMAIN:T_POLITICAL
+	1 => 群众
+	2 => 团员
+	3 => 预备党员
+	4 => 党员
+*/
+CREATE DOMAIN T_POLITICS AS INTEGER CHECK (VALUE IN (1, 2, 3, 4, 5));
+CREATE DOMAIN T_YEAR AS INTEGER CHECK (VALUE BETWEEN 1000 AND 9999 );
+CREATE DOMAIN T_EMAIL AS TEXT CHECK (VALUE LIKE '%@%.%');
+
+CREATE TABLE Role (
+	id                TEXT,
+	name              TEXT,
+	sex               T_ISOSEX,
+	politics          T_POLITICS,
+	photo_path        TEXT,
+	national_identity CHAR(18),
+	phone_number      TEXT,
+	cell_number       TEXT,
+	email             T_EMAIL,
+	_user_type        CHAR(1) NOT NULL,
+
+	password     TEXT NOT NULL DEFAULT '54321',       -- 加密后的密码
+
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE TestPlace (
+	place_id INTEGER PRIMARY KEY,
+	place_name TEXT
+);
+
+CREATE TABLE Student (
+	_user_type CHAR(1) DEFAULT 's',
+
+	sign_year         T_YEAR,      -- 注册年
+	graduate_school   TEXT,        -- 毕业学校
+	test_place        INTEGER REFERENCES   TestPlace(place_id) -- 考点
+) INHERITS (Role);
+
+CREATE TABLE Teacher (
+	_user_type CHAR(1) DEFAULT 't',
+
+	level TEXT      -- 职称
+) INHERITS(Role);
+
+
+INSERT INTO Student(id, name, sex, politics, password) VALUES(
+	'12345','宋江', 1, 1, '12345');
+
+INSERT INTO Student(id, name, sex, politics, password) VALUES(
+	'123456','清明', 1, 1, '123456');
+
+UPDATE Student SET email = 'shen@gmail.com',
+					phone_number = 123456,
+					cell_number = 88888,
+					national_identity = '123455676';
+
+COMMIT;
+
+
