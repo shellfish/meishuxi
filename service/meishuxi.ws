61d5baa295384cd6e0b23cff2bb41e02ca929406
zhousiyv <zhousiyv@2b567cfb-dbe9-4a7d-8032-5a92f511a253>
2009-11-19 03:14:41 +0000

    robust out dev-mainline
    git-svn-id: file:///mnt/libtree/archieve/svn/trunk@233 2b567cfb-dbe9-4a7d-8032-5a92f511a253

--- service/meishuxi.ws
+++ service/meishuxi.ws
@@ -1,4 +1,4 @@
-local root_path = '/home/zhousiyv/桌面/trunk/'
+local root_path = '/home/zhousiyv/桌面/trunk/service/'
 
 package.path = package.path  ..
 	';' .. root_path .. 'meishu/Lua/?.lua' ..
@@ -11,4 +11,6 @@ require "tr.wsapi_app"
 
 return	tr.wsapi_app.new{
 	SHOW_STACK_TRACE = true,
+	NODE_LOAD_PATH = root_path .. [[meishu/Node]],
+	OS_TYPE = 'Linux',
 }
--- ui/release/bootstrap.js
+++ ui/release/bootstrap.js
@@ -1,1 +1,99 @@
-/var/www/ui/meishuxi/bootstrap.js
+// dojo config
+var djConfig = {
+	//isDebug:true
+};
+
+
+var bootstrap =   (function() {
+
+	/****************** our configuration **********************************/
+	var config = {
+		dojoroot : '/js/dojo-svn',
+		dijittheme : 'soria', 
+		// tundra(冻土) soria(蓝色) a11y(辅助) nihilo(绿色)
+		service_url : '/service/meishuxi.ws'
+	}
+
+
+	/* some util func */
+	var head = document.getElementsByTagName('head')[0]
+
+	function loadJs( path )
+	{
+		s = document.createElement("script");	
+		s.type = 'text/javascript'
+		s.src = path
+		head.appendChild(s)
+	}
+
+	function loadCss( path )
+	{
+		s = document.createElement("link");
+		s.rel = 'stylesheet'
+		s.type = 'text/css'
+		s.href = path
+
+		head.appendChild(s)
+	}
+
+	// load dojo
+	loadJs(config.dojoroot + '/dojo/dojo.js')
+
+	// load source like dojo and its theme
+	loadCss(config.dojoroot + '/dojo/resources/dojo.css')
+	loadCss(config.dojoroot + '/dijit/themes/' + config.dijittheme + '/' + config.dijittheme + '.css')
+	
+	// dojox - toaster
+	loadCss(config.dojoroot + '/dojox/widget/Toaster/Toaster.css')
+
+
+
+	function load_app() {
+
+		// add theme class
+		dojo.addClass(dojo.body(), config.dijittheme)
+
+
+		// make a auto suitable baseUrl for index.html or sth
+		var baseUrl = dojo.global.location.pathname
+		baseUrl = baseUrl.replace(/^(.+)\/.*$/, '$1' + '\/')
+
+		loadCss(baseUrl + 'resource/slice.css')     // 相对于index.html
+
+		dojo.registerModulePath('lib', baseUrl + 'lib/')	
+
+		dojo.require('lib.core')
+		// 加入一些环境变量
+		lib.core.env = {
+			baseUrl:baseUrl,               // 基url
+			serviceUrl:config.service_url   // 后台服务url
+		}
+
+		// dojo.require('lib.test.testNav')
+		//dojo.require('lib.test.testLoginForm')
+		//dojo.require('lib.test.testHeader')
+		//dojo.require('lib.test.testRoot')
+		//dojo.require('lib.test.testDataTable')
+		//dojo.require('lib.test.testFileUpload')
+		//dojo.require('lib.test.testAjax')
+		//dojo.require('lib.test.testAlterPassword')
+		lib.core.bindApp() 
+	}
+
+	var flag = setInterval(function() {
+		if ( typeof(dojo) !== 'undefined') {
+			load_app()
+			clearInterval(flag)
+		}
+	}, 20)
+})
+
+// is IE
+if (navigator.userAgent.indexOf("MSIE")>0) {
+	window.onload = bootstrap
+}else {
+	console.log('browser is not IE>>>')
+	bootstrap()
+}
+
+
