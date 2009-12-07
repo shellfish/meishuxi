182212fea2a538bd807f1513be599bf6c7b418ae
zhousiyv <zhousiyv@zhousiyv-laptop.(none)>
2009-12-07 19:25:32 +0800

    minor fix

---
+++ external/share/cicala/all.lua
@@ -0,0 +1,8 @@
+require'cicala.persist.file'
+require'cicala.persist.database'
+require'cicala.persist.memcached'
+require'cicala.session'
+require'cicala.util'
+require'cicala.util.serialize'
+
+return cicala
