dfcdd1debafa98b1f2b5f58a537962defe158ea8
shellfish <shensizhefh@gmail.com>
2009-12-16 21:22:17 +0800

    add dojo-1.4.0(latest release)

--- runtime/bin/start_xavante.bat
+++ runtime/bin/start_xavante.bat
@@ -1,6 +1,6 @@
-@SETLOCAL LUA_PATH=%cd%\..\share\?.lua;%cd%\..\\share\?\init.lua
-@SETLOCAL LUA_CPATH=%cd%\..\lib\?.dll
+@SET LUA_PATH=%cd%\..\share\?.lua;%cd%\..\\share\?\init.lua
+@SET LUA_CPATH=%cd%\..\lib\?.dll
 
-@SETLOCAL LUA_PATH=%LUA_PATH%;%cd%\..\..\service\lua\?.lua;%cd%\..\..\service\lua\?\init.lua
+@SET LUA_PATH=%LUA_PATH%;%cd%\..\..\service\lua\?.lua;%cd%\..\..\service\lua\?\init.lua
 
 @lua.exe start_xavante.lua
